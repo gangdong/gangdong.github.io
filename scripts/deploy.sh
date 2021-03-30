320ccd55579158862ea81b8eba5c3ceb4c92ad70
gangdong <dqdongg@hotmail.com>
2021-03-30 09:10:17 +0800

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 # setup ssh-agent and provide the GitHub deploy key
-openssl openssl aes-256-cbc -K $encrypted_f217180e22ee_key -iv $encrypted_f217180e22ee_iv -in id_rsa.enc -out id_rsa -d
+openssl aes-256-cbc -K $encrypted_f217180e22ee_key -iv $encrypted_f217180e22ee_iv -in id_rsa.enc -out id_rsa -d
 # 对解密后的私钥添加权限
 chmod 600 id_rsa
 
